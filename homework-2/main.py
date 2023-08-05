cc503bf578684671c0b97994aaa92b82c2115f72
MaximSidorov1996 <131602849+MaximSidorov1996@users.noreply.github.com>
2023-08-05 11:51:36 +0300

    Add files via upload

--- homework-2/main.py
+++ homework-2/main.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     assert item.name == 'Смартфон'
 
     # длина наименования товара больше 10 символов
-    item.name = 'СуперСмартфон'
+    #item.name = 'СуперСмартфон'
     # Exception: Длина наименования товара превышает 10 символов.
 
     Item.instantiate_from_csv()  # создание объектов из данных файла
-    assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+    assert len(Item.all) == 6  # в файле 5 записей с данными по товарам + 1 объект до этого
 
-    item1 = Item.all[0]
-    assert item1.name == 'Смартфон'
+    item1 = Item.all[2]
+    assert item1.name == 'Ноутбук'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
